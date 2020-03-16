d75f7fca9e62e7357ae24c837011e719004c5e3a
Victor <victormeunier.dev@gmail.com>
2020-03-16 22:17:13 +0100

    add strafing

--- player.gd
+++ player.gd
@@ -10,10 +10,14 @@ func _ready():
 func get_input():
 	look_at(get_global_mouse_position())
 	velocity = Vector2()
-	if Input.is_action_pressed('down'):
+	if Input.is_action_pressed('ui_down'):
 		velocity = Vector2(-speed, 0).rotated(rotation)
-	if Input.is_action_pressed('up'):
+	if Input.is_action_pressed('ui_up'):
 		velocity = Vector2(speed, 0).rotated(rotation)
+	if Input.is_action_pressed('ui_right'):
+		velocity = Vector2(0, speed).rotated(rotation)
+	if Input.is_action_pressed('ui_left'):
+		velocity = Vector2(0, -speed).rotated(rotation)
 
 func _physics_process(delta):
 	get_input()
