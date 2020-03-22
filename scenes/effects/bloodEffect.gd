cda690e243bbf473c01a8d3b46397685674acc70
Victor <victormeunier.dev@gmail.com>
2020-03-22 03:17:55 +0100

    new effect

---
+++ scenes/effects/bloodEffect.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+onready var particle = $Particles2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	particle.emitting = true
+
+func _on_Timer_timeout():
+	queue_free()
