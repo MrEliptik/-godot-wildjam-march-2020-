088747ebd346ca0d13d5320ab2b4eea3baa687b6
Victor <victormeunier.dev@gmail.com>
2020-03-22 03:18:50 +0100

    update player gun

--- player.gd
+++ player.gd
@@ -1,13 +1,57 @@
 extends KinematicBody2D
 
 signal r_click(pos)
+signal hit_enemy(enemy)
 signal fired_shot
 
 export (int) var speed = 200
 
 var velocity = Vector2()
+var bullet_impact_pos = Vector2(0, 0)
+
+var close_to_object = false
+var bullet = 7
+var health = 100
+
+enum GUN{
+	NO,
+	GUN,
+	GUN_SUPPRESSED,
+	RIFLE
+}
+var current_gun = GUN.NO
+
+enum COLLECTABLES {
+	GUN,
+	GUN_SUPPRESSED,
+	RIFLE,
+	MED_KIT,
+	AMMO
+}
+
+var gun_node_sfx = {
+	GUN.GUN: "Gunshot",
+	GUN.GUN_SUPPRESSED: "Gunshot_suppressed",
+	GUN.RIFLE: "Gunshot_rifle"
+}
+
+var gun_node_sprite = {
+	GUN.GUN: "gun",
+	GUN.GUN_SUPPRESSED: "silencer",
+	GUN.RIFLE: "rifle"
+}
+
+var gun_power = {
+	GUN.GUN: 100,
+	GUN.GUN_SUPPRESSED: 80,
+	GUN.RIFLE: 180
+}
 
 func _ready():
+	pass
+	
+func _draw():
+	#draw_line(global_position, bullet_impact_pos, Color(0.8, 0, 0))
 	pass
 
 func get_input():
@@ -22,15 +66,37 @@ func get_input():
 	if Input.is_action_pressed('ui_left'):
 		velocity = Vector2(0, -speed).rotated(rotation)
 	if Input.is_action_just_pressed('ui_r_click'):
-		emit_signal("r_click", get_global_mouse_position())
-	if Input.is_action_just_pressed('ui_l_click') and $RayCast2D.is_colliding():
-		emit_signal("fired_shot", $RayCast2D.get_collision_point())
+		pass
+		#emit_signal("r_click", get_global_mouse_position())
+	if Input.is_action_just_pressed('ui_l_click'):
+		if current_gun == GUN.NO: return
+		
+		if bullet == 0: 
+			$Gunshot/Gunshot_empty.play()
+			return
+		bullet -= 1
+		
+		get_node('Gunshot/' + gun_node_sfx[current_gun]).play()
+		$Particles2D.emitting = true
+		
+		if $RayCast2D.is_colliding():
+			bullet_impact_pos = $RayCast2D.get_collision_point()
+			emit_signal("fired_shot", bullet_impact_pos)
+			#TODO: Calculate distance and delay impact ?
+			var collider = $RayCast2D.get_collider()
+			if collider is KinematicBody2D:
+				$Gunshot/Impact_body.play()
+				emit_signal('hit_enemy', collider)
+			elif collider is TileMap:
+				$Gunshot/Impact_concrete.play()
+			#update()
+		
 
 func _physics_process(delta):
 	get_input()
 
-	if velocity != Vector2(0, 0):
-		$AnimatedSprite.play("hold")
+	if velocity != Vector2(0, 0) and current_gun == GUN.NO:
+		$AnimatedSprite.play("hold_bw")
 		var one_is_playing = false
 		for footstep in $Footsteps.get_children():
 			if footstep.is_playing():
@@ -38,12 +104,21 @@ func _physics_process(delta):
 		if !one_is_playing:
 			pass
 			#$Footsteps.get_child(randi()%$Footsteps.get_child_count()).play()
+	elif current_gun == GUN.GUN:
+		$AnimatedSprite.play('gun_bw')
+	elif current_gun == GUN.GUN_SUPPRESSED:
+		$AnimatedSprite.play('silencer_bw')
 	else:
-		$AnimatedSprite.play("idle")
+		$AnimatedSprite.play("idle_bw")
 		
-	if $RayCast2D.is_colliding():
-		var enemy = $RayCast2D.get_collider()
-		print(enemy)
+	if velocity == Vector2(0, 0): $AnimationPlayer.stop()
+	else: $AnimationPlayer.play("move")
 			
 	velocity = move_and_slide(velocity)
 
+func on_pickup(object):
+	if object == GUN.GUN or object == GUN.GUN_SUPPRESSED or object == GUN.RIFLE:
+		current_gun = object
+		$Gunshot/Gun_pickup.play()
+	elif object == COLLECTABLES.AMMO:
+		$Gunshot/Ammo_pickup.play()
