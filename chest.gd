c56dd0a547b6676cae8942a88a75879685a407da
Victor <victormeunier.dev@gmail.com>
2020-03-22 03:16:43 +0100

    added chest

---
+++ chest.gd
@@ -0,0 +1,23 @@
+extends StaticBody2D
+
+signal opened(chest)
+
+onready var opened = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite.play('closed')
+
+func _on_Area2D_body_entered(body):
+	if opened: return
+	if body is StaticBody2D or body is TileMap: return
+	$AnimatedSprite.modulate = Color('fcf2c2')
+	$AnimatedSprite.play('opened')
+	$Open.play()
+	opened = true
+	emit_signal('opened', self)
+
+
+func _on_Area2D_body_exited(body):
+	if opened: return
+	$AnimatedSprite.modulate = Color('ffffff')
