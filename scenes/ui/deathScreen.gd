008aab3be0b12402ad949d3dbef4dbd51c67fff5
Victor <victormeunier.dev@gmail.com>
2020-03-23 23:23:50 +0100

    nes deathscreen

---
+++ scenes/ui/deathScreen.gd
@@ -0,0 +1,19 @@
+extends Control
+
+const normal_cursor = preload('res://art/crosshair.png')
+const hit_cursor = preload('res://art/crosshair_hit.png')
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	#Input.set_custom_mouse_cursor(hit_cursor, CURSOR_POINTING_HAND)
+
+func _on_Button_mouse_entered():
+	Input.set_custom_mouse_cursor(hit_cursor)
+
+func _on_Button_mouse_exited():
+	Input.set_custom_mouse_cursor(normal_cursor)
+
+func _on_Button_pressed():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
