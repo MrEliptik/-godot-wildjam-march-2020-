a06abe7d54618120a16b1daf8be4b4e5b7bbf54b
Victor <victormeunier.dev@gmail.com>
2020-03-24 23:28:40 +0100

    end animation + rooms

--- game.gd
+++ game.gd
@@ -8,12 +8,15 @@ const bomb = preload('res://bomb.tscn')
 const normal_cursor = preload('res://art/crosshair.png')
 const hit_cursor = preload('res://art/crosshair_hit.png')
 const death_screen = preload('res://scenes/ui/deathScreen.tscn')
+const win_screen = preload('res://scenes/ui/winScreen.tscn')
 
 const BOMB_DAMAGE = 300
 const MED_KIT_HEAL = 50
 
 export var hit_effect: PackedScene
 export var blood_effect: PackedScene
+
+var hit_count = 0
 
 onready var player = $Player
 
@@ -40,7 +43,12 @@ var objects = {
 	"Chest_1": [COLLECTABLES.AMMO],
 	"Chest_2": [COLLECTABLES.BOMB, COLLECTABLES.AMMO],
 	"Chest_3": [COLLECTABLES.MED_KIT],
-	"Chest_4": [COLLECTABLES.BOMB, COLLECTABLES.AMMO]
+	"Chest_4": [COLLECTABLES.BOMB, COLLECTABLES.AMMO],
+	"Chest_5": [COLLECTABLES.AMMO, COLLECTABLES.BOMB],
+	"Chest_6": [COLLECTABLES.MED_KIT],
+	"Chest_7": [COLLECTABLES.AMMO],
+	"Chest_8": [COLLECTABLES.BOMB, COLLECTABLES.MED_KIT],
+	"Chest_9": [COLLECTABLES.AMMO, COLLECTABLES.MED_KIT]
 }
 
 # Called when the node enters the scene tree for the first time.
@@ -130,8 +138,7 @@ func on_player_chest(chest):
 			$CanvasLayer/HUD/Bullets.visible = true
 			$CanvasLayer/HUD.set_notification('+ 7 bullets', $CanvasLayer/HUD.ICONS.AMMO)
 		elif obj == COLLECTABLES.MED_KIT:
-			player.health += MED_KIT_HEAL
-			if player.health > 100: player.health = 100
+			player.take_heal(MED_KIT_HEAL)
 			$CanvasLayer/HUD.set_health(player.health)
 			$CanvasLayer/HUD.set_notification('+ '+str(MED_KIT_HEAL)+' health' , $CanvasLayer/HUD.ICONS.HEART)
 		elif obj == COLLECTABLES.BOMB:
@@ -141,6 +148,7 @@ func on_player_chest(chest):
 			$CanvasLayer/HUD.set_notification('+ 3 bombs' , $CanvasLayer/HUD.ICONS.BOMB)
 
 func on_player_die():
+	$CanvasLayer/HUD.set_health(player.health)
 	get_tree().paused = true
 	$CanvasLayer.add_child(death_screen.instance())
 
@@ -162,8 +170,7 @@ func on_enemy_loot(obj):
 		$CanvasLayer/HUD/Bullets.visible = true
 		$CanvasLayer/HUD.set_notification('+ 3 bullets', $CanvasLayer/HUD.ICONS.AMMO)
 	elif obj == COLLECTABLES.MED_KIT:
-		player.health += MED_KIT_HEAL
-		if player.health > 100: player.health = 100
+		player.take_heal(MED_KIT_HEAL)
 		$CanvasLayer/HUD.set_health(player.health)
 		$CanvasLayer/HUD.set_notification('+ '+ str(MED_KIT_HEAL) +' health', $CanvasLayer/HUD.ICONS.HEART)
 	elif obj == COLLECTABLES.BOMB:
@@ -176,12 +183,49 @@ func on_bomb_explode(bomb):
 	for body in bomb.get_node('AreaExplosion').get_overlapping_bodies():
 		if body is KinematicBody2D:
 			if body.get_parent().name == 'BombContainer': return
-			body.health -= BOMB_DAMAGE
 			if body.name == 'Player':
+				player.take_damage(BOMB_DAMAGE)
 				$CanvasLayer/HUD.set_health(player.health)
+			else: body.health -= BOMB_DAMAGE
 		if body is StaticBody2D:
 			if body.get_parent().name == 'ChestsContainer': return
 			body.destroy()
 
 func on_destructable_destroyed(destructable):
 	$DestructableContainer.remove_child(destructable)
+
+func _on_EndArea_body_entered(body):
+	if body.name == "Player":
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+		player.get_node('BodyLight').enabled = false
+		player.get_node('Camera2D').zoom += Vector2(0.2, 0.2)
+		player.win()
+		$AnimationPlayer.play('end')
+		# 18 times - 4 sec pause
+		$Hit2.play()
+		$Timer.start()
+		# 19 - sec pause
+
+func _on_SlowArea_body_entered(body):
+	if body.name == 'Player':
+		$Hit.play()
+		player.speed = 70
+		player.get_node('Camera2D').zoom -= Vector2(0.2, 0.2)
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == 'end':
+		$CanvasLayer.add_child(win_screen.instance())
+		$Hit3.play()
+		$CanvasLayer/WinScreen/AnimationPlayer.play('win')
+		player.get_node('Camera2D').zoom += Vector2(0.2, 0.2)
+		$Timer2.start()
+
+func _on_Timer_timeout():
+	hit_count += 1
+	$Hit2.play()
+	if hit_count < 18:
+		$Timer.start()
+
+func _on_Timer2_timeout():
+	$Tween.interpolate_property(player.get_node("Camera2D"), 'zoom', player.get_node('Camera2D').zoom, player.get_node('Camera2D').zoom - Vector2(0.6, 0.6), 2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()
